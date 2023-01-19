069ceaaad89bcc7a69037f4997a4e40416cc334c
iliacodes <6ac177ed@protonmail.com>
2023-01-19 15:24:36 -0800

    created a loading page and panels + functionality to exercise

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,12 +1,73 @@
 import React, { Component } from "react";
+import Loading from './Loading'
 
 import classnames from "classnames";
 
-class Dashboard extends Component {
-  render() {
-    const dashboardClasses = classnames("dashboard");
 
-    return <main className={dashboardClasses} />;
+const data = [
+  {
+    id: 1,
+    label: "Total Interviews",
+    value: 6
+  },
+  {
+    id: 2,
+    label: "Least Popular Time Slot",
+    value: "1pm"
+  },
+  {
+    id: 3,
+    label: "Most Popular Day",
+    value: "Wednesday"
+  },
+  {
+    id: 4,
+    label: "Interviews Per Day",
+    value: "2.3"
+  }
+];
+
+
+
+
+class Dashboard extends Component {
+  state ={
+    loading: true,
+    focused: null
+  }
+
+  selectPanel(id) {
+    this.setState({
+      focused: id
+    });
+  }
+
+  render() {
+    const dashboardClasses = classnames("dashboard", {
+      "dashboard--focused": this.state.focused
+    });
+
+    if(this.state.loading) {
+      return <Loading />
+    }
+
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+    .map(panel => (
+     <Panel
+      key={panel.id}
+      id={panel.id}
+      label={panel.label}
+      value={panel.value}
+      focused={this.state.focused === panel.id}
+      onSelect={this.selectPanel}
+     />
+    ));
+
+    return (
+    <main className={dashboardClasses}>
+      {panels}
+    </main> 
+    );
   }
 }
 
